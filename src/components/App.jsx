7b8946385be065c1222cd86ae437df8d6bd28cda
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-02-08 15:25:44 +0200

    added lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import { Container } from './Container/Container.styled';
-import HomePage from 'pages/HomePage/HomePage';
-import CatalogPage from 'pages/CatalogPage/CatalogPage';
-import FavoritesPage from 'pages/FavoritesPage/FavoritesPage';
+import { lazy } from 'react';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const CatalogPage = lazy(() => import('pages/CatalogPage/CatalogPage'));
+const FavoritesPage = lazy(() => import('pages/FavoritesPage/FavoritesPage'));
 
 export const App = () => {
   return (
